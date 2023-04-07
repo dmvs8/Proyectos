482a3d094c6ccfc06e16f1d32b7f313e1b99666a
Diego Vergara <129889857+dmvs8@users.noreply.github.com>
2023-04-07 01:02:30 -0300

    Add files via upload

---
+++ Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PrimerParcialPOO.Core
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public TipoInstrumento Tipo { get; set; }
+    }
+}
